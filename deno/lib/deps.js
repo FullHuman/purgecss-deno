fe9c2ad0f8311dc32ebd8f9f0eaf96c87260cdd4
Ffloriel <florielfedry@gmail.com>
2021-08-01 19:03:01 +0100

    fix: add glob deno/node interop

--- deno/lib/deps.js
+++ deno/lib/deps.js
@@ -1,11 +1,33 @@
-import { access, readFile } from "https://deno.land/std@0.98.0/node/fs.ts";
-import { resolve } from "https://deno.land/std@0.98.0/node/path.ts";
-import { promisify } from "https://deno.land/std@0.98.0/node/util.ts";
+import { expandGlobSync } from "https://deno.land/std@0.103.0/fs/mod.ts";
+import { access, readFile } from "https://deno.land/std@0.103.0/node/fs.ts";
+import { resolve } from "https://deno.land/std@0.103.0/node/path.ts";
+import { promisify as prom } from "https://deno.land/std@0.103.0/node/util.ts";
 import selectorParser from "https://deno.land/x/postcss_selector_parser@v6.0.0/mod.js";
 import postcss from "https://deno.land/x/postcss/mod.js";
 
 export const fs = { access, readFile };
 export const path = { resolve };
-export const util = { promisify };
+export const promisify = prom;
 export const selectorParser = selectorParser;
 export const postcss = postcss;
+
+export const glob = {
+  sync: (pattern, options) => {
+    const filePaths = [];
+
+    for (const file of expandGlobSync(pattern, {
+      root: options?.root,
+      exclude:
+        typeof options?.ignore === "string"
+          ? [options.ignore]
+          : options?.ignore,
+      includeDirs: !options?.nodir,
+      extended: !options?.noext,
+      globstar: !options?.noglobstar,
+      caseInsensitive: !options?.nocase,
+    })) {
+      filePaths.push(file.path);
+    }
+    return filePaths;
+  },
+};
--- deps.js
+++ deps.js
@@ -1,11 +1,18 @@
-import { access, readFile } from "https://deno.land/std@0.98.0/node/fs.ts";
-import { resolve } from "https://deno.land/std@0.98.0/node/path.ts";
-import { promisify } from "https://deno.land/std@0.98.0/node/util.ts";
+import { access, readFile } from "https://deno.land/std@0.103.0/node/fs.ts";
+import { resolve } from "https://deno.land/std@0.103.0/node/path.ts";
+import { promisify as prom } from "https://deno.land/std@0.103.0/node/util.ts";
 import selectorParser from "https://deno.land/x/postcss_selector_parser@v6.0.0/mod.js"
 import postcss from "https://deno.land/x/postcss/mod.js"
 
 export const fs = { access, readFile };
 export const path = { resolve };
-export const promisify = promisify;
+export const promisify = prom;
 export const selectorParser = selectorParser;
 export const postcss = postcss;
+export const glob = {
+  sync: (globPattern, globOptions) => {
+    for (const file of expandGlobSync(globPattern)) {
+      console.log(file);
+    }
+  }
+}
