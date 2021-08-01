e2e033078a16d66f43ffe3fcd1707dfae584afb6
Ffloriel <florielfedry@gmail.com>
2021-08-01 19:55:08 +0100

    fix: update postcss-selector-parser

--- deno/lib/deps.ts
+++ deno/lib/deps.ts
@@ -2,7 +2,7 @@ import { expandGlobSync } from "https://deno.land/std@0.103.0/fs/mod.ts";
 import { access, readFile } from "https://deno.land/std@0.103.0/node/fs.ts";
 import { resolve } from "https://deno.land/std@0.103.0/node/path.ts";
 import { promisify as prom } from "https://deno.land/std@0.103.0/node/util.ts";
-import selectorParserImport from "https://deno.land/x/postcss_selector_parser@v6.0.0/mod.js";
+import selectorParserImport from "https://deno.land/x/postcss_selector_parser@v6.0.1/mod.js";
 import postcssImport from "https://deno.land/x/postcss@8.3.6/mod.js";
 
 export const fs = { access, readFile };
